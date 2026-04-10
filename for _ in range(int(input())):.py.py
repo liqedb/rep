cf83ac365542f6ecddbec01d374ec5cd7bd9175b
liqedb <257537209+liqedb@users.noreply.github.com>
2026-04-10 16:11:55 +0300

    шаман

---
+++ for _ in range(int(input())):.py.py
@@ -0,0 +1,3 @@
+for _ in range(int(input())):
+    name, *food = input().split()
+    print(food)
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-print('hello')
-print('gay')
-print('hello')
-print('gay')
+casha = dict()
+for _ in range(int(input())):
+    name, *food = input().split()
+    for x in food:
+        if x not in casha:
+            casha[x] = [name]
+        else:
+            casha[x].append(name)
+z = input()
+if z not in casha:
+    print('Таких нет')
+else:
+    print(*sorted(casha[z]), sep='\n')
